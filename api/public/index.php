5be7a608586fa550361a63fa758c1dfd7e357f45
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-05-02 17:38:17 +0200

    nouveau router + modif bdd + route post pour les bien

--- api/public/index.php
+++ api/public/index.php
@@ -6,7 +6,35 @@ use App\Controller\ArticleController;
 define("ROOT", dirname(__DIR__));
 require ROOT . "/vendor/autoload.php";
 
+if (isset($_SERVER["PATH_INFO"])) {
+    $path = explode("/",$_SERVER["PATH_INFO"]);
+    if (isset($path[3])) {
+        $controllerName = "App\Controller\\".ucfirst($path[3]). "Controller";
+        $controller = new $controllerName;
+        switch ($_SERVER["REQUEST_METHOD"]) {
+            case "GET":
+                if (isset($path[4]) && is_numeric($path[4])) {
+                    $controller->single($path[4]);
+                } else {
+                    $controller->index();
+                }
+                break;
+            case "POST":
+                $controller->save();
+                break;
+            case "PUT" || "PATCH":
+                echo "i égal 2";
+                break;
+            case "DELETE":
+                echo "i égal 3";
+                break;
+        }
+    }
+} else {
+    // TODO: erreur si pas la route
+}
 
+/*
 if (isset($_SERVER["PATH_INFO"])) {
     $path = explode("/",$_SERVER["PATH_INFO"]);
     if (isset($path[3])) {
@@ -23,5 +51,5 @@ if (isset($_SERVER["PATH_INFO"])) {
         }
     }
 } else {
-
-}
+    // TODO: erreur si pas la route
+}*/
