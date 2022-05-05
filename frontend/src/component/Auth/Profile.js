a6b603bc1d11917280081edcd68b6f61901b6b4c
nolan78 <superpika.nolan@gmail.com>
2022-05-05 11:32:05 +0200

    création page profile

---
+++ frontend/src/component/Auth/Profile.js
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { useNavigate } from "react-router-dom"
+import authService from '../../services/auth.service';
+
+const PaperProps = {
+	elevation: 0,
+	sx: {
+		overflow: 'visible',
+		filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+		mt: 1.5,
+		'& .MuiAvatar-root': {
+			width: 32,
+			height: 32,
+			ml: -0.5,
+			mr: 1,
+		},
+		'&:before': {
+			content: '""',
+			display: 'block',
+			position: 'absolute',
+			top: 0,
+			right: 14,
+			width: 10,
+			height: 10,
+			bgcolor: 'background.paper',
+			transform: 'translateY(-50%) rotate(45deg)',
+			zIndex: 0,
+		},
+	}
+}
+
+export default function AccountMenu() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const navigate = useNavigate();
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	function handleClickLogout(e) {
+		authService.logout();
+	};
+
+
+	return (
+		<>
+			<Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+				<Tooltip title="Compte">
+					<IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+						<Avatar sx={{ width: 32, height: 32 }}>NS</Avatar>
+					</IconButton>
+				</Tooltip>
+			</Box>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				onClick={handleClose}
+				PaperProps={PaperProps}
+				transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+				anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+			>
+				<MenuItem onClick={() => navigate('/inscription')}>
+					Inscrire un agent
+				</MenuItem>
+				<Divider />
+				<MenuItem onClick={handleClickLogout}>
+					Déconnexion
+				</MenuItem>
+			</Menu>
+		</>
+	);
+}
