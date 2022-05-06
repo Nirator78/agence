d42ef65c0c357c79a142e3b02fb4f18e773b28a6
nolan78 <superpika.nolan@gmail.com>
2022-05-06 15:01:54 +0200

    Front

--- frontend/src/pages/Bien.js
+++ frontend/src/pages/Bien.js
@@ -21,46 +21,52 @@ function Bien(props) {
     const user = authService.getUser();
 
     return (
-        <div className="grid md:grid-cols-3 sm:grid-cols-2">
-            <div className="rounded overflow-hidden shadow-lg" xs={4} key={bien.id}>
-            {
-                   bien?.images?.map((item) => {  
-                        return (
-                            <div className="flex">
-                                <img className="max-w-sm" src={  item.url} alt="Sunset in the mountains"></img>
-                            </div>
-                        )
-                })
-            }
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{bien.titre}</div>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.prix} €</span>
-                    <p className="text-gray-700 text-base">
-                    Type : {bien.type_bien}
-                    </p>          
-                    <p className="text-gray-700 text-base">
-                    Disponible a {bien.type_achat}
-                    </p>                        
-                    <p className="text-gray-700 text-base">
-                    {bien.description}
-                    </p>
+        <div className="w-full shadow-lg rounded overflow-hidden m-4">
+            <div className="w-4/5 m-auto flex justify-between items-center " key={bien.id}>
+                <div className="w2/4 flex justify-center items-center">
+                {
+                    bien?.images?.map((item) => {  
+                            return (
+            
+                                <img className="max-w-sm" src={item.url} alt=""></img>
+                    
+                            )
+                    })
+                }
                 </div>
-                <div className="px-6">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.nbPiece} pièces</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.superficie} m²</span>
-                    {bien.piscine ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Piscine </span> : <></>}
-                    {bien.cheminee ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Cheminée </span> : <></>}
-                    {bien.balcon ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Balcon </span> : <></>}
-                    {bien.terrasse ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Terrasse </span> : <></>}
-                </div>
-                <RendezVous bien={bien.id}/>
+                <div className="w2/4 flex justify-center items-center">
+                    <div>
+                        <div className="font-bold text-xl mb-2">
+                            {bien.titre}
+                        </div>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.prix} €</span>
+                        <p className="text-gray-700 text-base">
+                        Type : {bien.type_bien}
+                            </p>          
+                        <p className="text-gray-700 text-base">
+                            Disponible a {bien.type_achat}
+                        </p>                        
+                        <p className="text-gray-700 text-base">
+                            {bien.description}
+                        </p>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.nbPiece} pièces</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{bien.superficie} m²</span>
+                        {bien.piscine ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Piscine </span> : <></>}
+                        {bien.cheminee ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Cheminée </span> : <></>}
+                        {bien.balcon ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Balcon </span> : <></>}
+                        {bien.terrasse ? <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> Terrasse </span> : <></>}
+                        <RendezVous bien={bien.id}/>
+                    </div>
+                </div> 
+            </div>
+            <div>
                 {(user?.role === "admin" || user?.id === bien.user_id) && (
                 <div className='flex'>
-                <FormBien id={bien.id} />         
-                    <button className="flex py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-700 hover:bg-red-600" onClick={async () => await bienService.deleteBien(bien.id) &&  navigate(`/bien`) }>
-                        Supprimer  
-                    </button> 
-                </div>  
+                    <FormBien id={bien.id} />         
+                        <button className="flex py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-700 hover:bg-red-600" onClick={async () => await bienService.deleteBien(bien.id) &&  navigate(`/bien`) }>
+                            Supprimer  
+                        </button> 
+                    </div>  
                 )}  
             </div>
         </div>
